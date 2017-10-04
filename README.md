39684334b0d3164b02da321bed101c5ad9fab298
Devin Cunningham <devincunningham@allegropediatrics.com>
2017-10-03 20:33:45 -0700

    Moved Q1 into function

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # about_me
+Devin updated Q's 1-7 into functions then pushed up to GitHub 10/03/17 8:31pm
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 var userPoints = 0;
 console.log('Hello there!');
@@ -10,41 +10,47 @@ alert('Hi ' + user + '! I am so glad you want to play my game. Please answer the
 console.log('User name is ' + user + '.');
 
 //Question 1
-var answer1 = prompt('Do I have a passion for web development? Type yes or no.').toLowerCase();
-console.log('The user answered: ' + answer1);
+function questionOne(){
+  var answer1 = prompt('Do I have a passion for web development? Type yes or no.').toLowerCase();
+  console.log('The user answered: ' + answer1);
 
-if (answer1 === 'yes' || answer1 === 'Y') {
-userPoints++;
-  alert('I do! I love digging into code to make things look and work right. You now have ' + userPoints + ' point(s).');
-} else {
-  alert('Wrong! I am super passionate about building multi-functional websites utilizing video and graphics. You now have ' + userPoints + ' point(s).');
-  console.log('The user has ' + userPoints + ' point(s)');
+  if (answer1 === 'yes' || answer1 === 'Y') {
+    userPoints++;
+    alert('I do! I love digging into code to make things look and work right. You now have ' + userPoints + ' point(s).');
+  } else {
+    alert('Wrong! I am super passionate about building multi-functional websites utilizing video and graphics. You now have ' + userPoints + ' point(s).');
+    console.log('The user has ' + userPoints + ' point(s)');
+  }
 }
+questionOne();
 
 //Question 2
-var answer2 = prompt('Am I an entrepeneur? Type yes or no.').toUpperCase();
-console.log('The user answered: ' + answer2);
+function questionTwo(){
+  var answer2 = prompt('Am I an entrepeneur? Type yes or no.').toUpperCase();
+  console.log('The user answered: ' + answer2);
 
-if (answer2 === 'YES' || answer2 === 'Y') {
-userPoints++;
-  alert ('You\'re right! I love having something I can put energy into and that I\'m passionate about! You now have ' + userPoints + ' point(s).');
-} else {
-  alert ('Wrong, ' + user + '. I am super passionate about my business! You now have ' + userPoints + ' pont(s).');
-  console.log('After question 3, the user has ' + userPoints + ' point(s)');
+  if (answer2 === 'YES' || answer2 === 'Y') {
+    userPoints++;
+    alert ('You\'re right! I love having something I can put energy into and that I\'m passionate about! You now have ' + userPoints + ' point(s).');
+  } else {
+    alert ('Wrong, ' + user + '. I am super passionate about my business! You now have ' + userPoints + ' pont(s).');
+    console.log('After question 3, the user has ' + userPoints + ' point(s)');
+  }
 }
 
 //Question3
-var answer3 = prompt('Do I speak a foreign language? Type yes or no.').toLowerCase();
-console.log('The user answered: ' + answer3);
+function questionThree(){
+  var answer3 = prompt('Do I speak a foreign language? Type yes or no.').toLowerCase();
+  console.log('The user answered: ' + answer3);
 
-if (answer3 === 'yes' || answer3 === 'Y') {
-  userPoints++;
-  alert('Woohoo you guessed right! I speak spanish and love speaking it as much as possible when I travel. Again, I like a challenge! You now have ' + userPoints + ' point(s)');
-} else {
-  alert('Wrong again ' + user + '! Language and culture is another big passion of mine. You now have ' + userPoints + ' point(s).');
-  console.log('After question 3, the user has ' + userPoints + ' point(s).');
+  if (answer3 === 'yes' || answer3 === 'Y') {
+    userPoints++;
+    alert('Woohoo you guessed right! I speak spanish and love speaking it as much as possible when I travel. Again, I like a challenge! You now have ' + userPoints + ' point(s)');
+  } else {
+    alert('Wrong again ' + user + '! Language and culture is another big passion of mine. You now have ' + userPoints + ' point(s).');
+    console.log('After question 3, the user has ' + userPoints + ' point(s).');
+  }
 }
-
 //Question 4
 var answer4 = prompt('Do I like cats more than dogs? Type yes or no.').toUpperCase();
 console.log(answer4);
@@ -70,11 +76,11 @@ if (answer5 === 'yes' || answer5 === 'Y') {
 }
 
 //Question 6
-var attempts= 4;
-var answer6= prompt('How many foreign countries have I travelled too? Please enter the numerical value. You will have 4 attempts.')
+var attempts = 4;
+var answer6 = prompt('How many foreign countries have I travelled too? Please enter the numerical value. You will have 4 attempts.');
 while (attempts > 1) {
   if (answer6 < 5) {
-    attempts= attempts - 1;
+    attempts = attempts - 1;
     answer6 = prompt('That is too low! Try again. \nHow many foreign countries have I travelled too?\nYou have ' + attempts + ' attempt(s) left.');
     console.log('The user guessed ' + answer6 + ' and has ' + attempts + ' attempts left');
   } else if (answer6 > 5) {
@@ -97,33 +103,34 @@ if (attempts = 1) {
 }
 
 //Question 7
-var favoriteHolidays = ['christmas' , 'thanksgiving' , 'halloween' , 'easter' , 'new years'];
-var arrayLength = favoriteHolidays.length; // Item in array
-var attemptsHolidays = 5;
-
-var answer7 = prompt('Can you guess one of my favorite holidays?').toLowerCase();
-console.log('question7');
-
-while  (attemptsHolidays > 0) {
-  for (var counter = 0; counter < favoriteHolidays.length; counter++) {
-    if (answer7 === favoriteHolidays[counter]) {
-      alert('You got one right!');
-      console.log('The user got question 7 right.')
-      userPoints++;
-  }
-}
-if (answer7 === ) {
-  userPoints++;
-    alert('That is correct! ' + answer7 + ' is on my list of favorite holidays!');
-    console.log('The user guessed ' + answer7 + ' and has ' + (attemptsHolidays) + ' attempt(s) left.');
-  } else {
-  attemptsHolidays--;
-    answer7 = prompt('Wrong answer, ' + answer7 + ' is not on my list of favorite holidays.\nYou have ' + attemptsHolidays + '  attempts left.');
-    console.log('The user guessed ' + answer7 + ' and has ' + (attemptsHolidays) + ' attempt(s) left.');
-  }
-  alert('My favorite holidays are:\n' + favoriteHolidays + '\n' + user + ' , you scored ' + userPoints + ' out of 7 points.');
-  console.log('After question 7 the user has ' + userPoints + ' point(s).');
-}
+// var favoriteHolidays = ['christmas' , 'thanksgiving' , 'halloween' , 'easter' , 'new years'];
+// var arrayLength = favoriteHolidays.length; // Item in array
+// var attemptsHolidays = 5;
+//
+// var answer7 = prompt('Can you guess one of my favorite holidays?').toLowerCase();
+// console.log('question7');
+//
+// while  (attemptsHolidays > 0) {
+//   for (var counter = 0; counter < favoriteHolidays.length; counter++) {
+//     if (answer7 === favoriteHolidays[counter]) {
+//       alert('You got one right!');
+//       console.log('The user got question 7 right.');
+//       userPoints++;
+//     }
+//   }
+// }
+// if (answer7 === favoriteHolidays){
+//   userPoints++;
+//     alert('That is correct! ' + answer7 + ' is on my list of favorite holidays!');
+//     console.log('The user guessed ' + answer7 + ' and has ' + (attemptsHolidays) + ' attempt(s) left.');
+//   } else {
+//   attemptsHolidays--;
+//     answer7 = prompt('Wrong answer, ' + answer7 + ' is not on my list of favorite holidays.\nYou have ' + attemptsHolidays + '  attempts left.');
+//     console.log('The user guessed ' + answer7 + ' and has ' + (attemptsHolidays) + ' attempt(s) left.');
+//   }
+//   alert('My favorite holidays are:\n' + favoriteHolidays + '\n' + user + ' , you scored ' + userPoints + ' out of 7 points.');
+//   console.log('After question 7 the user has ' + userPoints + ' point(s).');
+// }
 
 /*Conclusion
 if (userPoints === 7) {
